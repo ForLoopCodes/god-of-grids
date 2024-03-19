ca20ec220bd26eedae8afbb99c0241994e2ab016
forloopcodes <meetnp1706@gmail.com>
2024-03-20 03:29:26 +0530

    game of life

--- src/components/Grid.jsx
+++ src/components/Grid.jsx
@@ -48,31 +48,39 @@ export default function Grid() {
     }, t2);
   };
 
-  const glowElementInstant = (x, y, t) => {
+  const glowElementForever = (x, y) => {
     const element = ref.current.children[x * columns + y] || null;
     if (!element) return;
     element.classList.add("bg-cyan-300");
     element.classList.remove("bg-neutral-800");
-
-    setTimeout(() => {
-      element.classList.remove("bg-cyan-300");
-      element.classList.add("bg-neutral-800");
-    }, t);
   };
-
+  const unGlowAll = () => {
+    // unglow only elements in liveCells
+    for (let i = 0; i < rows; i++) {
+      for (let j = 0; j < columns; j++) {
+        const element = ref.current.children[i * columns + j] || null;
+        if (!element) return;
+        element.classList.add("bg-neutral-800");
+        element.classList.remove("bg-cyan-300");
+        element.classList.remove("bg-cyan-500");
+      }
+    }
+  };
   const matrixRain = () => {
-    let density = 200;
-    let theta = 1;
+    let density = 500;
+    let tanTheta = 1;
+    let rainSpeed = 100;
+    let maxDropLength = 1500;
     const fall = setInterval(() => {
       let newRandomElementInRow1 = Math.floor(Math.random() * columns);
-      let time1 = 100;
-      let time2 = Math.floor(100 + Math.random() * 1500);
+      let time1 = rainSpeed;
+      let time2 = Math.floor(rainSpeed + Math.random() * maxDropLength);
       glowElement(0, newRandomElementInRow1, time1, time2);
       for (let i = 1; i < rows; i++) {
         setTimeout(() => {
           glowElement(
             i,
-            newRandomElementInRow1 + Math.floor(i * theta),
+            newRandomElementInRow1 + Math.floor(i * tanTheta),
             time1,
             time2
           );
@@ -85,110 +93,154 @@ export default function Grid() {
   };
 
   setTimeout(() => {
-    matrixRain();
+    // matrixRain();
   }, 1000);
-  const [liveCells, setLiveCells] = useState([
-    { x: 0, y: 0 },
-    { x: 0, y: 1 },
-    { x: 0, y: 2 },
-    { x: 1, y: 0 },
-    { x: 1, y: 1 },
-    { x: 1, y: 2 },
-    { x: 2, y: 0 },
-    { x: 2, y: 1 },
-    { x: 2, y: 2 },
-  ]);
-  localStorage.setItem("liveCells", JSON.stringify(liveCells));
+  //FORLOOP
+  let liveCells = [
+    "0 0",
+    "0 1",
+    "0 2",
+    "0 4",
+    "0 5",
+    "0 6",
+    "0 8",
+    "0 9",
+    "0 10",
+    "0 12",
+    "0 16",
+    "0 17",
+    "0 18",
+    "0 20",
+    "0 21",
+    "0 22",
+    "0 24",
+    "0 25",
+    "0 26",
+    "1 0",
+    "1 4",
+    "1 6",
+    "1 8",
+    "1 10",
+    "1 12",
+    "1 16",
+    "1 18",
+    "1 20",
+    "1 22",
+    "1 24",
+    "1 26",
+    "2 0",
+    "2 1",
+    "2 2",
+    "2 4",
+    "2 6",
+    "2 8",
+    "2 9",
+    "2 10",
+    "2 12",
+    "2 16",
+    "2 18",
+    "2 20",
+    "2 22",
+    "2 24",
+    "2 25",
+    "2 26",
+    "3 0",
+    "3 4",
+    "3 6",
+    "3 8",
+    "3 9",
+    "3 12",
+    "3 16",
+    "3 18",
+    "3 20",
+    "3 22",
+    "3 24",
+    "4 0",
+    "4 4",
+    "4 5",
+    "4 6",
+    "4 8",
+    "4 10",
+    "4 12",
+    "4 13",
+    "4 14",
+    "4 16",
+    "4 17",
+    "4 18",
+    "4 20",
+    "4 21",
+    "4 22",
+    "4 24",
+  ];
+  const showLiveCells = (liveCells) => {
+    unGlowAll();
+    liveCells.forEach((cell) => {
+      const x = parseInt(cell.split(" ")[0]);
+      const y = parseInt(cell.split(" ")[1]);
+      glowElementForever(x, y);
+    });
+  };
+  const killCells = (arr) => {
+    arr.forEach((cell) => {
+      const x = parseInt(cell.split(" ")[0]);
+      const y = parseInt(cell.split(" ")[1]);
+      liveCells = liveCells.filter((c) => c !== `${x} ${y}`);
+      showLiveCells(liveCells);
+    });
+  };
+  const reviveCells = (arr) => {
+    arr.forEach((cell) => {
+      const x = parseInt(cell.split(" ")[0]);
+      const y = parseInt(cell.split(" ")[1]);
+      liveCells = liveCells.concat(`${x} ${y}`);
+      showLiveCells(liveCells);
+    });
+  };
   const gameOfLife = () => {
     let rate = 500;
-    const generation = setInterval(() => {
-      const liveCellsFromStorage = JSON.parse(
-        localStorage.getItem("liveCells")
-      );
-      // render live cells for 1s and update the live cells every 1s
-      for (let i = 0; i < liveCellsFromStorage.length; i++) {
-        glowElement(
-          liveCellsFromStorage[i].x,
-          liveCellsFromStorage[i].y,
-          rate - 50,
-          rate - 51
-        );
-      }
-      for (let i = 0; i < rows; i++) {
-        for (let j = 0; j < columns; j++) {
-          let neighbors = [
-            { x: i + 1, y: j },
-            { x: i - 1, y: j },
-            { x: i, y: j + 1 },
-            { x: i, y: j - 1 },
-            { x: i + 1, y: j + 1 },
-            { x: i - 1, y: j - 1 },
-            { x: i + 1, y: j - 1 },
-            { x: i - 1, y: j + 1 },
-          ];
-          let liveNeighbors = 0;
-          for (let k = 0; k < neighbors.length; k++) {
-            for (let l = 0; l < liveCellsFromStorage.length; l++) {
-              if (
-                liveCellsFromStorage[l].x === neighbors[k].x &&
-                liveCellsFromStorage[l].y === neighbors[k].y
-              ) {
-                liveNeighbors++;
-              }
-            }
-          }
-          //show live neighbors of each cell in the grid
-          const element = ref.current.children[i * columns + j] || null;
-          if (!element) return;
-          element.textContent = liveNeighbors;
-
-          const isCellLive = (x, y) => {
-            for (let w = 0; w < liveCellsFromStorage.length; w++) {
-              let cell = liveCellsFromStorage[w];
-              if (cell.x === x && cell.y === y) {
-                return true;
-              }
-            }
-            return false;
-          };
-
-          if (
-            isCellLive(i, j) &&
-            (liveNeighbors < 2 || liveNeighbors > 3) &&
-            liveCellsFromStorage.length > 0
-          ) {
-            //console.log("removing cell", i, j);
-            //console.log("liveCells", liveCellsFromStorage);
-            localStorage.setItem(
-              "liveCells",
-              JSON.stringify(
-                liveCellsFromStorage.filter(
-                  (cell) => !(cell.x == i && cell.y == j)
-                )
-              )
-            );
-          } else {
-            if (liveNeighbors === 3 && isCellLive(i, j) === false) {
-              // prevent duplicates
-              //console.log("adding cell", i, j);
-              localStorage.setItem(
-                "liveCells",
-                JSON.stringify(liveCellsFromStorage.concat({ x: i, y: j }))
-              );
-              //console.log("liveCells", liveCellsFromStorage);
+    showLiveCells(liveCells);
+    setInterval(() => {
+      let toKill = [];
+      let toRevive = [];
+      liveCells.forEach((cell) => {
+        const x = parseInt(cell.split(" ")[0]);
+        const y = parseInt(cell.split(" ")[1]);
+        let liveNeighbours = 0;
+        for (let i = -1; i < 2; i++) {
+          for (let j = -1; j < 2; j++) {
+            if (i === 0 && j === 0) continue;
+            if (liveCells.includes(`${x + i} ${y + j}`)) {
+              liveNeighbours++;
             }
           }
         }
+        if (liveNeighbours < 2 || liveNeighbours > 3) {
+          toKill.push(cell);
+        }
+      });
+      for (let i = 0; i < rows; i++) {
+        for (let j = 0; j < columns; j++) {
+          let liveNeighbours = 0;
+          for (let k = -1; k < 2; k++) {
+            for (let l = -1; l < 2; l++) {
+              if (k === 0 && l === 0) continue;
+              if (liveCells.includes(`${i + k} ${j + l}`)) {
+                liveNeighbours++;
+              }
+            }
+          }
+          if (liveNeighbours === 3) {
+            toRevive.push(`${i} ${j}`);
+          }
+        }
       }
+      killCells(toKill);
+      reviveCells(toRevive);
     }, rate);
-    window.addEventListener("resize", () => {
-      clearInterval(generation);
-    });
   };
   setTimeout(() => {
-    // gameOfLife();
+    gameOfLife();
   }, 1000);
-
   return (
     <div>
       <div
